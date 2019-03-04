70061635fa47ff6e2fb29d2d4ca9d13e46234515
Sokureo <pelmenium.sokurium@gmail.com>
2019-03-04 20:00:37 +0300

    whole dict

---
+++ make_json.py
@@ -0,0 +1,44 @@
+import json
+
+f = open('rutul_data.csv', 'r', encoding='utf-8')
+data = f.readlines()
+del data[0]
+#print(len(data))
+
+res = dict()
+res['data'] = list()
+
+adds = dict()
+toDel = list()
+
+for indx, line in enumerate(data):
+    data[indx] = line.split(';')
+    
+for i, new in enumerate(data[1:]):
+    #print(data[i])
+    #print(new)
+    if new[1] == data[i][1]:
+        adds[data[i][2]] = new[2] + ' (' + new[10].split('_')[1] + ')'
+        toDel.append(i+1)
+        #print(i+1)
+
+
+for ind, els in enumerate(data):
+    #els = line.split(';')
+    if ind not in toDel:
+        row = dict()
+        row['id'] = els[0]
+        row['transcription'] = els[2]
+        row['pos'] = els[14].strip()
+        if els[3]:
+            row['pos'] = row['pos'] + ', ' + els[3] + ' class'
+        row['audio'] = "<audio controls preload=\"none\"><source src=\"audio/index.wav\" type=\"audio/wav\"></audio>".replace('index', els[0])
+        row['translation'] = els[10].split('_')[0]
+        row['rustranslation'] = els[8].split('_')[0]
+        if els[2] in adds:
+            row['add'] = adds[els[2]]
+            row['add_audio'] = ''
+        res['data'].append(row)
+        
+with open('data.json', 'w', encoding='utf-8') as fw2:
+    json.dump(res, fp=fw2, ensure_ascii=False, indent=1)
